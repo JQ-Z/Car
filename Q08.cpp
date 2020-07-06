59b768b26a028e95a11a046025220885c9ece475
JQ-Z <ZJQ6106@gmail.com>
2020-07-06 21:22:24 +0800

    Q08 Q09 Q10 puzzle

--- Q08.cpp
+++ Q08.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 using namespace std;
 
+int *(*map);
+int answer = 0;
+int endstep;
+void dfs(int x, int y, int step)
+{
+	if(step == endstep){
+		answer++;
+		return;
+	}
+	for(int i = 1; i <= 4;i++){
+		int nx = x, ny = y;
+		switch(i){
+			case 1: nx += 1;break;
+			case 2: nx -= 1;break;
+			case 3: ny += 1;break;
+			case 4: ny -= 1;break;
+		}
+		if(map[nx][ny] == 1)continue;
+		map[nx][ny] = 1;
+		dfs(nx,ny,step + 1);
+		map[nx][ny] = 0;
+	}
+}
+
 int main()
 {
-
+	cin >> endstep;
+	map = new int*[25];
+	for(int i = 0; i <= 24; i++){
+		map[i] = new int[25];
+		for(int j = 0; j <= 24; j++)
+			map[i][j] = 0;
+	}
+	map[12][12] = 1;
+	dfs(12,12,0);
+	cout << answer << endl;
 	return 0;
 }
--- Q09.cpp
+++ Q09.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main()
 {
 	int arr[21][11] = {0};
-	for(int i = 0 ; i <= 20; i++)arr[i][0] = 1;
-	for(int i = 1; i <= 20; i++)
-		for(int j = 1; j <= 10; j++)
+	arr[0][0] = 1;
+	for(int i = 0; i <= 20; i++)
+		for(int j = 0; j <= 10; j++)
 		{
-			if(j == i ||  j + 10 == i)continue;
-			cout << "arr[" << i-1 << "]" << "[" << j << "]=" << arr[i-1][j];
-			cout << "   arr[" << i << "]" << "[" << j-1 << "]=" << arr[i][j-1];
-			arr[i][j] = arr[i-1][j] + arr[i][j-1];
-			cout << "   arr[" << i << "]" << "[" << j << "]=" << arr[i][j] << endl;
+			// if(j == i ||  j + 10 == i)continue;
+			// cout << "arr[" << i-1 << "]" << "[" << j << "]=" << arr[i-1][j];
+			// cout << "   arr[" << i << "]" << "[" << j-1 << "]=" << arr[i][j-1];
+			// arr[i][j] = arr[i-1][j] + arr[i][j-1];
+			// cout << "   arr[" << i << "]" << "[" << j << "]=" << arr[i][j] << endl;
+			if(j != 0 && j == i) continue;
+			if(j != 0 && j + 10 == i) continue;
+			if(i - 1 > 0 ) arr[i][j] += arr[i-1][j];
+			if(j - 1 > 0 ) arr[i][j] += arr[i][j-1];
 		}
 	cout << arr[20][9] + arr[19][10] << endl;
 	return 0;
